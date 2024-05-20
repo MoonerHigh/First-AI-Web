e2491d03b1e40c99b491ee2a2c533068f5a63584
MoonerHigh <Maozilox@Gmail.com>
2024-05-20 22:56:59 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "node_modules/preline/dist/*.js",
+  ],
+  plugins: [
+      require('preline/plugin'),
+  ],
+}
