4ce78115bc658244db348755c07d1587fbb6b40f
MoonerHigh <Maozilox@Gmail.com>
2024-05-25 02:09:50 +0800

    feat:index页面重构，newChat页面模板

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,14 @@ import Home from "../views/index.vue";
 // 创建路由实例
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/", component: Home }],
+  routes: [
+    { path: "/", component: Home },
+    {
+      path: "/newChat:message",
+      name: "newChat",
+      component: () => import("../views/newChat.vue"),
+    },
+  ],
 });
 
 export const setupRouter = (app) => {
